80f51bdeea926c541915ab95cd918cd10e66a4f9
Sina Dabiri <sina.dabiri@gladstone.ucsf.edu>
2019-10-26 12:03:27 -0700

    Using distance transform to improve watershed segmentation.

--- Watershed.py
+++ Watershed.py
@@ -17,13 +17,13 @@ plt.figure("Watershed Segmentation")
 
 # noise removal
 kernel = np.ones((3, 3), np.uint8)
-opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=1)
+opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=5)
 # plt.subplot(1, 4, 1)
 # plt.imshow(opening)
 # plt.title('Morphological Opening')
 
 # finding sure background area
-sure_bg = cv.dilate(opening, kernel, iterations=3)
+sure_bg = cv.dilate(opening, kernel, iterations=1)
 plt.subplot(2, 3, 1)
 plt.imshow(sure_bg)
 plt.title('Dilation:sure_bg')
@@ -31,7 +31,7 @@ plt.title('Dilation:sure_bg')
 # distance transform to find sure foreground area
 dist_transform = cv.distanceTransform(opening, cv.DIST_L2, 5)
 print("dist_transform is: ", dist_transform.max())
-ret, sure_fg = cv.threshold(dist_transform, 0.4*dist_transform.max(), 255, 0)
+ret, sure_fg = cv.threshold(dist_transform, 0.2*dist_transform.max(), 255, 0)
 plt.subplot(2, 3, 2)
 plt.imshow(dist_transform)
 plt.title('dist transform')
@@ -41,15 +41,17 @@ plt.title('thresholds: sure_fg')
 
 # Finding unknown region
 sure_fg = np.uint8(sure_fg)
+dist_transform = np.uint8(dist_transform)
 print(sure_fg.dtype)
 print(sure_bg.dtype)
-unknown = cv.subtract(sure_bg, sure_fg)
+# unknown = cv.subtract(sure_bg, sure_fg)
+unknown = cv.subtract(sure_bg, dist_transform)
 plt.subplot(2, 3, 4)
 plt.imshow(unknown)
 plt.title('unknown')
 
 # mark Labeling
-ret, markers = cv.connectedComponents(sure_fg)
+ret, markers = cv.connectedComponents(dist_transform, labels=None, connectivity=8)
 # Adding one to sure background, so it is 1 instead of 0
 markers = markers + 1
 # marking the unknown region as zero
